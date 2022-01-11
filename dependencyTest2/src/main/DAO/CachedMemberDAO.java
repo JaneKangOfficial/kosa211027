b160b2a9c730a5b2e9cc58e2d9557e08e3c108d4
user <dev_k9@naver.com>
2022-01-11 18:05:54 +0900

    20220111 2.의존객체 주입

---
+++ dependencyTest2/src/main/DAO/CachedMemberDAO.java
@@ -0,0 +1,15 @@
+package main.DAO;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import main.DTO.MemberDTO;
+/*
+ * 의존객체 문제점 : 의존객체명 변경시 하나하나 바꿔야 한다.
+ * 문제점 해결을 위해 의존객체 주입
+ */
+public class CachedMemberDAO extends MemberDAO{
+
+}
